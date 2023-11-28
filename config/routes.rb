eb77f907d7e6f2b3cfba759ded8c6756d9ee5d68
west0818 <21cc060a@rikkyo.ac.jp>
2023-11-28 20:33:44 +0900

    自由にわかりやすいメッセージ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get 'posts/northibaraki' => 'posts#northibaraki'
+  get 'posts/southibaraki' => 'posts#southibaraki'
+  get 'posts/westibaraki' => 'posts#westibaraki'
+  get 'posts/centralibaraki' => 'posts#centralibaraki'
+  get 'posts/rokkouibaraki' => 'posts#rokkouibaraki'
+  get 'posts/food' => 'posts#food'
+  get 'posts/sightseeing' => 'posts#sightseeing'
+  get 'posts/index' => 'posts#index'
   resources :topics
   resources :topic2s
   resources :topic3s  
---
+++ test/controllers/northibaraki_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class NorthibarakiControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
