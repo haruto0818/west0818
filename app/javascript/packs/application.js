0932aee63d5b1357d3786edc36a4040faa2294cb
west0818 <21cc060a@rikkyo.ac.jp>
2023-11-28 22:34:38 +0900

    自由にわかりやすいメッセージ

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,52 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+    // ふわっ
+    $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+      var elemPos = $(this).offset().top -50;//要素より、50px上の
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight){
+      $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+      }else{
+      $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+      }
+      });
+      // ふわっ
+    $('.fadeLeftTrigger').each(function(){ //fadeUpTriggerというクラス名が
+      var elemPos = $(this).offset().top -50;//要素より、50px上の
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight){
+      $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+      }else{
+      $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+      }
+      });
+}
+  // 画面をスクロールをしたら動かしたい場合の記述
+    $(window).scroll(function (){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面をスクロールをしたら動かしたい場合の記述
+  // 画面が読み込まれたらすぐに動かしたい場合の記述
+    $(window).on('load', function(){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function (){
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function(){
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
